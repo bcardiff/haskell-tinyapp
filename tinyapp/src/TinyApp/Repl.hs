fb8bdce5532c27b3b3428ef607a162dad3729ed2
Brian J. Cardiff <bcardiff@gmail.com>
2024-06-18 10:33:52 -0300

    Add documentation

--- tinyapp/src/TinyApp/Repl.hs
+++ tinyapp/src/TinyApp/Repl.hs
@@ -1,25 +1,30 @@
+-- | Build REPL apps
 module TinyApp.Repl where
 
 import Control.Exception
 import Control.Monad
 import System.IO
 
+-- | Signals whether the REPL should continue asking input from the user or exit.
 data ContinueExit = Continue | Exit
   deriving (Eq, Show)
 
+-- | Defines a REPL application that is not allows to perform arbitrary IO while executing.
 data Sandbox state = Sandbox
-  { -- Initial state
+  { -- | Initial state
     initialize :: state,
-    -- The prompt to show. It can depend on the state
+    -- | The prompt to show. It can depend on the state
     prompt :: state -> String,
-    -- Process the user input given the current state
+    -- | Process the user input given the current state
     -- Returns the next state, the output and whether to continue or not the program
     update :: String -> state -> (state, String, ContinueExit)
   }
 
+-- | Executes the REPL application.
 runRepl :: Sandbox s -> IO ()
 runRepl = Control.Monad.void . runRepl'
 
+-- | Executes the REPL application returning its final state.
 runRepl' :: forall s. Sandbox s -> IO s
 runRepl' config =
   let go :: s -> IO s
